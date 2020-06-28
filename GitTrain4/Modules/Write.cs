8ffd89180c808edc92d1681f775d873b4f524ac6
Aurélien BOUDOUX <aurelien@boudoux.fr>
2020-06-28 19:41:31 +0200

    toolbox write color

---
+++ GitTrain4/Modules/Write.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GitTrain4
+{
+	public static class Write
+	{
+		public static void Color(string data, ConsoleColor color)
+		{
+			Console.ForegroundColor = color;
+			Console.WriteLine(data);
+			Console.ForegroundColor = ConsoleColor.White;
+		}
+	}
+}
